4dcafc939883fca603c498c64b1655fae5489c7d
K. <kayo@illumium.org>
2024-01-11 00:38:55 +0500

    Simplify tokio implementation

--- tokio/src/lib.rs
+++ tokio/src/lib.rs
@@ -19,17 +19,12 @@ pub use hidg_core::{
 
 use core::marker::PhantomData;
 use std::{
-    io,
-    os::unix::io::{AsRawFd, RawFd},
+    fs::File,
+    io::{ErrorKind, Read, Write},
     path::Path,
-    pin::Pin,
-    task::{Context, Poll},
 };
 
-use tokio::{
-    io::{unix::AsyncFd, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, ReadBuf},
-    task::spawn_blocking,
-};
+use tokio::{io::unix::AsyncFd, task::spawn_blocking};
 
 async fn asyncify<F, T>(f: F) -> Result<T>
 where
@@ -38,87 +33,13 @@ where
 {
     match spawn_blocking(f).await {
         Ok(res) => res,
-        Err(_) => Err(Error::new(io::ErrorKind::Other, "background task failed")),
-    }
-}
-
-#[doc(hidden)]
-pub struct File {
-    // use file to call close when drop
-    inner: AsyncFd<std::fs::File>,
-}
-
-impl File {
-    pub fn from_file(file: std::fs::File) -> Result<Self> {
-        Ok(Self {
-            inner: AsyncFd::new(file)?,
-        })
-    }
-}
-
-impl AsRawFd for File {
-    fn as_raw_fd(&self) -> RawFd {
-        self.inner.as_raw_fd()
-    }
-}
-
-impl AsyncRead for File {
-    fn poll_read(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &mut ReadBuf<'_>,
-    ) -> Poll<Result<()>> {
-        loop {
-            use std::io::Read;
-
-            let mut guard = match self.inner.poll_read_ready(cx) {
-                Poll::Ready(x) => x,
-                Poll::Pending => return Poll::Pending,
-            }?;
-
-            match guard.try_io(|inner| inner.get_ref().read(buf.initialize_unfilled())) {
-                Ok(Ok(bytes_read)) => {
-                    buf.advance(bytes_read);
-                    return Poll::Ready(Ok(()));
-                }
-                Ok(Err(err)) => {
-                    return Poll::Ready(Err(err));
-                }
-                Err(_would_block) => continue,
-            }
-        }
-    }
-}
-
-impl AsyncWrite for File {
-    fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<Result<usize>> {
-        use std::io::Write;
-
-        loop {
-            let mut guard = match self.inner.poll_write_ready(cx) {
-                Poll::Ready(x) => x,
-                Poll::Pending => return Poll::Pending,
-            }?;
-
-            match guard.try_io(|inner| inner.get_ref().write(buf)) {
-                Ok(result) => return Poll::Ready(result),
-                Err(_would_block) => continue,
-            }
-        }
-    }
-
-    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<()>> {
-        Poll::Ready(Ok(()))
-    }
-
-    fn poll_shutdown(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<()>> {
-        Poll::Ready(Ok(()))
+        Err(_) => Err(Error::new(ErrorKind::Other, "background task failed")),
     }
 }
 
 /// HID Gadget Device
 pub struct Device<C: Class> {
-    file: File,
+    file: AsyncFd<File>,
     _class: PhantomData<C>,
 }
 
@@ -126,8 +47,7 @@ impl<C: Class> Device<C> {
     /// Open device by path or name
     pub async fn open(path: impl AsRef<Path>) -> Result<Self> {
         let path = path.as_ref().to_owned();
-        let file = asyncify(move || open(path, false)).await?;
-        let file = File::from_file(file)?;
+        let file = AsyncFd::new(asyncify(move || open(path, false)).await?)?;
         Ok(Self {
             file,
             _class: PhantomData,
@@ -139,8 +59,9 @@ impl<C: Class> Device<C> {
     where
         C::Input: AsRef<[u8]>,
     {
+        let _ = self.file.writable().await?;
         let raw = input.as_ref();
-        let len = self.file.write(raw).await?;
+        let len = self.file.get_ref().write(raw)?;
 
         check_write(len, raw.len())
     }
@@ -150,8 +71,9 @@ impl<C: Class> Device<C> {
     where
         C::Output: AsMut<[u8]>,
     {
+        let _ = self.file.readable().await?;
         let raw = output.as_mut();
-        let len = self.file.read(raw).await?;
+        let len = self.file.get_ref().read(raw)?;
 
         check_read(len, raw.len())?;
 
